047aaa2a96a938e9c25518353e686cb4ca809e72
tiagopotencia <tiagomourabrandao@gmail.com>
2016-11-05 08:14:21 -0200

    Implementa página de voos

--- app/app.js
+++ app/app.js
@@ -6,14 +6,15 @@ import {HomePage} from './pages/home/home';
 import {Cities} from './providers/cities/cities';
 import {Places} from './providers/places/places';
 import {Hoteis} from './providers/hoteis/hoteis'; 
-import {Roteiro} from './providers/roteiro/roteiro'; 
+import {Roteiro} from './providers/roteiro/roteiro';
+import {Flights} from './providers/flights/flights'; 
 
 @App({
   template: '<ion-nav id="my-nav" [root]="rootPage"></ion-nav>',
   config: {
     mode: 'md'
   }, // http://ionicframework.com/docs/v2/api/config/Config/
-  providers: [ANGULAR2_GOOGLE_MAPS_PROVIDERS, Cities, Places, Hoteis, Roteiro]
+  providers: [ANGULAR2_GOOGLE_MAPS_PROVIDERS, Cities, Places, Hoteis, Roteiro, Flights]
 })
 export class MyApp {
   static get parameters() {
